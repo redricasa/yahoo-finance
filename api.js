ab1e6cee79f940590f1585424609b767ffdfb2b9
Frederica Blissett aka Samri <redricasa@users.noreply.github.com>
2020-08-05 13:50:30 -0700

    added dotenv config code in order to use the api-key and api-url in the api.js

--- api.js
+++ api.js
@@ -1,8 +1,11 @@
 const axios = require("axios");
-
+// configuring the dotenv so that I can use process.env
+const dotenv = require('dotenv');
+dotenv.config();
+// AXIOS syntax to get response from rapid api 
 axios({
     "method":"GET",
-    "url":"https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/get-charts",
+    "url":process.env.API_URL,
     "headers":{
         "content-type":"application/octet-stream",
         "x-rapidapi-host":"apidojo-yahoo-finance-v1.p.rapidapi.com",
--- config.js
+++ config.js
@@ -2,5 +2,6 @@
 const dotenv = require('dotenv');
 dotenv.config();
 module.exports = {
-    masterKey: process.env.API_KEY,
+    API_KEY: process.env.API_KEY,
+    port: process.env.PORT
 };
