c2ffda181bfdd6690da5fb58ab043e8e15af6ae7
Frederica Blissett aka Samri <redricasa@users.noreply.github.com>
2020-08-22 21:41:01 -0700

    since this app is an html app, there's no node functionality. Therefore, the .env.js file is created to mimic what the dotenv module does by having the key and value set up in json format. Traversed through the response object to get the opening and closing quotes and time in 15 minute inclements

--- README.md
+++ README.md
@@ -9,3 +9,6 @@ Yahoo Finance API app that displays Amazon's stock information in real time
 > - You'll need to go to rapid api website and sign up for an account. Then go to Yahoo Finance API and subscribe.
 > - It'll give you an api key which you should put in your environmental variables- in a .env file. 
 > - Make sure to also create a .gitignore file and include .env before you commit the API addition!
+* the time displayed is in whatever time zone that your computer's time zone is.
+## TODO: 
+- create a react app in order to have a node app where I can use node modules
--- api.js
+++ api.js
@@ -1,30 +1,25 @@
-const axios = require("axios");
+// const axios = require("axios");
 // configuring the dotenv so that I can use process.env
-const dotenv = require('dotenv');
-const result= dotenv.config();
-
+// const dotenv = require('dotenv');
+// const result= dotenv.config();
 // AXIOS syntax to get response from rapid api 
-axios({
-    "method":"GET",
-    "url":process.env.API_URL,
-    "headers":{
-        "content-type":"application/json",
-        "x-rapidapi-host":"apidojo-yahoo-finance-v1.p.rapidapi.com",
-        "x-rapidapi-key":process.env.API_KEY,
-        "useQueryString":true
-    },
-    "params":{
-        "region":"US",
-        "lang":"en",
-        "symbol":"APPL",
-        "interval":"5m",
-        "range":"1d"
-    }
-})
-.then((response)=>{
-    console.log('RESPONSE-------------------- /n', response)
-})
-.catch((error)=>{
-    console.log('ERROR--------------------  /n', error)
-})
-
+function getData(symbol_string){
+    // return a promise
+    return axios({
+        "method":"GET",
+        "url":process.env.API_URL,
+        "headers":{
+            "content-type":"application/json",
+            "x-rapidapi-host":"apidojo-yahoo-finance-v1.p.rapidapi.com",
+            "x-rapidapi-key":process.env.API_KEY,
+            "useQueryString":true
+        },
+        "params":{
+            "region":"US",
+            "lang":"en",
+            "symbol":symbol_string,
+            "interval":"15m",
+            "range":"1d"
+        }
+    })
+}
